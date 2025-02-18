be1419ca206f9ec313ae609980d29545af326c6d
JayCoker92 <joshuaolowo888@gmail.com>
2025-02-18 01:23:56 -0800

    features add me

--- app.js
+++ app.js
@@ -1,41 +1,54 @@
 "use strict";
-let inpOfGuess = document.querySelector(".guess");
+
+ let inpOfGuess = document.querySelector(".guess");
 let checkBtn = document.querySelector(".btn-checker");
 let message = document.querySelector(".message");
 let number = document.querySelector(".number");
 let score = document.querySelector(".score")
+let again = document.querySelector(".again")
 
 function gamer(e) {
-    e.preventDefault();
-    let randomNumGen = Math.ceil(Math.random() * 6);
-    let body = document.body.style;
-    console.log(randomNumGen);
-  
-    if (!inpOfGuess.value) {
-      message.textContent = "input field must not be empty";
-    } else if (inpOfGuess.value > randomNumGen) {
-      message.textContent = "you guessed higher";
-      number.textContent = randomNumGen;
-      score.textContent--
-      body.backgroundColor = "blue"
-    } else if (inpOfGuess.value < randomNumGen){
-    message.textContent = "you guessed lower";
+  e.preventDefault();
+  let randomNumGen = Math.ceil(Math.random() * 6);
+  let body = document.body.style;
+  console.log(randomNumGen);
+
+  if (!inpOfGuess.value) {
+    message.textContent = "input field must not be empty";
+  } else if (inpOfGuess.value > randomNumGen) {
+    message.textContent = "you guessed higher";
     number.textContent = randomNumGen;
-    score.textContent --;
-    body.backgroundColor = "yellow"
-    }else if (Number(inpOfGuess.value) === randomNumGen){
-        message.textContent = "you guessed right👍🏿 ";
-        number.textContent = randomNumGen;
-        body.backgroundColor = "green";
-    }
-    if (score.textContent < 1) {
-        checkBtn.setAttribute("disabled","disabled");
-        message.textContent = "game over, restart 👎🏿 ";
-        checkBtn.style.backgroundColor = "grey";
-        checkBtn.style.cursor = "no drop"
-    }
-
-
+    score.textContent--
+    body.backgroundColor = "blue"
+  }else if(inpOfGuess.value < randomNumGen){
+    message.textContent = "you guess lower";
+    number.textContent = randomNumGen;
+    score.textContent--;
+    body.backgroundColor = "yellow";
+  }else if(Number(inpOfGuess.value) === randomNumGen){
+    message.textContent = "you guess right 👍🏿";
+    number.textContent = randomNumGen;
+    body.backgroundColor = "green";
   }
-  
-  checkBtn.addEventListener("click", gamer);
+  if (score.textContent < 1){
+    checkBtn.setAttribute("disabled", "disabled");
+    message.textContent = "game over restart 👎🏿";
+    checkBtn.style.backgroundColor = "grey";
+    checkBtn.style.cursor = "no-drop";
+  }
+}
+
+checkBtn.addEventListener("click", gamer);
+
+function resetgame(){
+  score.textContent = "6";
+  number.textContent = "?";
+  message.textContent = "start guessing...";
+  inpOfGuess.value = "";
+  document.body.style.backgroundColor = "red";
+  checkBtn.removeAttribute("disabled");
+  checkBtn.style.backgroundColor = "";
+  checkBtn.style.cursor = "pointer";
+}
+again.addEventListener("click",resetgame)
+
